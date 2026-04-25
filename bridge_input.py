65113df100b29f42185c5d63465263e2edde6f39
FarhanJouhar <278893769+FarhanJouhar@users.noreply.github.com>
2026-04-25 10:01:01 +0530

    Added patient input script named bridge_input,py
    Initial logic to capture the patient's name and age. It is barebones for now.

---
+++ bridge_input.py
@@ -0,0 +1,6 @@
+# The python script for handling data input.
+patient_name = str(input("Enter the patient's name: "))
+# This will be my first line of code. A very simple string input for the patient's name.
+patient_age = int(input("Enter the patient's age: "))
+print(f"Patient Name: {patient_name}")
+print(f"Patient Age: {patient_age}")
