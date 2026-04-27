10e4316ae24f7e06224812da6a5146c7802bfa7e
FarhanJouhar <278893769+FarhanJouhar@users.noreply.github.com>
2026-04-27 21:09:09 +0530

    Update bridge_input.py

--- bridge_input.py
+++ bridge_input.py
@@ -1,17 +1,19 @@
 # The python script for handling data input.
-patient_name = str(input("Enter the patient's name: "))
-# This will be my first line of code. A very simple string input for the patient's name.
-#Following loop will ensure that only valid integers and positive numbers are accepted into age.
 while True:
-  try:
-    patient_age = int(input("Enter the patient's age: "))
-    if patient_age > 0:
-      break
-    else:
-      print("Please enter a real age!")
-  except ValueError: 
-    print("Please enter a number!")
-print("="*30)
-print(f"Patient Name: {patient_name}")
-print(f"Patient Age: {patient_age}")
-print("="*30)
+  patient_name = str(input("Enter the patient's name: "))
+  if patient_name == "exit":
+    break
+#Following loop will ensure that only valid integers and positive numbers are accepted into age.
+  while True:
+    try:
+      patient_age = int(input("Enter the patient's age: "))
+      if patient_age > 0:
+        break
+      else:
+        print("Please enter a real age!")
+    except ValueError: 
+      print("Please enter a number!")
+  print("="*30)
+  print(f"Patient Name: {patient_name}")
+  print(f"Patient Age: {patient_age}")
+  print("="*30)
