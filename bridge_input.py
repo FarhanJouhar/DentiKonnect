6f7cbe87d50ecd6ebfe94c22214dd0a62a40a1dd
FarhanJouhar <278893769+FarhanJouhar@users.noreply.github.com>
2026-04-28 20:55:05 +0530

    Update bridge_input.py

--- bridge_input.py
+++ bridge_input.py
@@ -1,7 +1,11 @@
 # The python script for handling data input.
+Patient_ID = 1000
 while True:
   patient_name = str(input("Enter the patient's name: "))
-  if patient_name == "exit":
+  if not patient_name:
+    print("Name cannot be blank!") #Names can no longer be blank
+    continue
+  if patient_name.lower() == "exit": #eXiT can be typed in any way
     break
 #Following loop will ensure that only valid integers and positive numbers are accepted into age.
   while True:
@@ -13,7 +17,9 @@ while True:
         print("Please enter a real age!")
     except ValueError: 
       print("Please enter a number!")
+  Patient_ID +=1
   print("="*30)
-  print(f"Patient Name: {patient_name}")
+  print(f"Patient ID: {Patient_ID}")
+  print(f"Patient Name: {patient_name.title()}")
   print(f"Patient Age: {patient_age}")
   print("="*30)
